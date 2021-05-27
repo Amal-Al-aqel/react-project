29a658221c01b6a8a094e2b79839afcbe192c343
your-name <your-email@example.com>
2021-05-27 11:31:45 +0300

    first commit

--- src/Img.js
+++ src/Img.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
 export default function Img(props) {
-    console.log(props.imgSrc);
     return (
-        <div>
-            <img src={props.imgSrc}/>
-        </div>
+<div class="card" style={{width: "18rem;"}}>
+<img src={props.imgSrc} class="card-img-top" alt="" height="300"/> 
+<div class="card-body">
+    <h5 class="card-title">{props.title}</h5>
+    <p class="card-text">{props.desc}</p>
+    <a href="#" class="btn btn-warning">Recipe</a>
+</div>
+</div>   
     )
 }
